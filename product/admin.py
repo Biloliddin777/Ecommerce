005d76bba7219c14a6c0bb2f802f52b6305009df
Biloliddin <biloliddin14042009@gmail.com>
2024-08-23 23:24:28 +0500

    Google, Import and Export

--- product/admin.py
+++ product/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
-
 from product.models import Product, ProductAttribute, AttributeValue, Attribute, Image
+from import_export.admin import ImportExportModelAdmin
+
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(ProductAttribute)
-admin.site.register(AttributeValue)
-admin.site.register(Attribute)
-admin.site.register(Image)
+@admin.register(Product)
+class ProductAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(ProductAttribute)
+class ProductAttributeAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(AttributeValue)
+class AttributeValueAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Attribute)
+class AttributeAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(ImportExportModelAdmin):
+    pass
--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
 from user.models import User
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(ImportExportModelAdmin):
+    pass
